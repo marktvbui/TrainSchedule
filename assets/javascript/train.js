80bdcbc78582c4aa596397f9d0e2fb96634e5666
Mark Bui <tvyybui@yahoo.com>
2017-06-04 02:52:12 -0700

    working on backend

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -9,10 +9,21 @@ $(document).ready.(function(){
     console.log(destination);
     console.log(time);
     console.log(frequency);
-    $('#train-display').td(trainName);
-    $('#destination-display').td(destination);
-    $('#time-display').td(time);
-    $('#frequency-display').td(frequency);
+
+    var table = document.getElementById("display-table");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = trainName;
+    cell2.innerHTML = destination;
+    cell3.innerHTML = time;
+    cell4.innerHTML = frequency;
+    $('#train-display').html(trainName);
+    $('#destination-display').html(destination);
+    $('#time-display').html(time);
+    $('#frequency-display').html(frequency);
 
     localStorage.clear();
     localStorage.setItem("localTrain", trainName);
@@ -21,8 +32,8 @@ $(document).ready.(function(){
     localStorage.setItem('localFrequency', frequency);
   });
 
-  $("#train-display").td(localStorage.getItem("localTrain"));
-  $("#destination-display").td(localStorage.getItem("localDestination"));
-  $("#time-display").td(localStorage.getItem("localTime"));
-  $("#frequency-display").td(localStorage.getItem("localFrequency"));
+  $("#train-display").html(localStorage.getItem("localTrain"));
+  $("#destination-display").html(localStorage.getItem("localDestination"));
+  $("#time-display").html(localStorage.getItem("localTime"));
+  $("#frequency-display").html(localStorage.getItem("localFrequency"));
 });
