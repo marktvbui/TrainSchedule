9e69aad4a6776b024e22778d70208c0e77a4d80a
Mark Bui <tvyybui@yahoo.com>
2017-06-05 01:23:15 -0700

    removed <td> tags from html, creating them dynamically in js file

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,31 +1,39 @@
 $(document).ready(function(){
+  var saveData = [];
+  var data = {};
   $("#add-train").on("click", function(event) {
     event.preventDefault();
     //setting variables, table to target table from index
     var table = document.getElementById("display-table");
+    var row = $('<tr>');
     //these variables saves the value from the input fields
     var trainName = $('#train-input').val().trim();
     var destination = $('#destination-input').val().trim();
     var time = $('#time-input').val().trim();
     var frequency = $('#frequency-input').val().trim();
-    // console.log(trainName);
-    // console.log(destination);
-    // console.log(time);
-    // console.log(frequency);
-    //displaying input fields into table
-    $('#train-display').html(trainName);
-    $('#destination-display').html(destination);
-    $('#time-display').html(time);
-    $('#frequency-display').html(frequency);
+    //saving hash (key, value)
+    data.Train = trainName;
+    data.Destination = destination;
+    data.Time = time;
+    data.Frequency = frequency;
+    //pushing the hash into the array
+    saveData.push(data);
+    console.log(saveData);
+    // displaying input fields into table
+      row.append($('<td>').html(trainName));
+      row.append($('<td>').html(destination));
+      row.append($('<td>').html(time));
+      row.append($('<td>').html(frequency));
+      $('#display-table').append(row);
     // for (var i = 0; i < 10; i ++) {
-    //   var insertTable = document.getElementById('display-table').insertRow(i);
+    //   var Table = document.getElementById('display-table').insertRow(i);
     //   for (var j = 0; j < 10; j ++) {
-    //     var insert = insertTable.insertCell(j);
+    //     var insert = Table.insertCell(j);
     //   }
-    //   table.rows[i].cells[0].append = trainName;
-    //   table.rows[i].cells[1].append = destination;
-    //   table.rows[i].cells[2].append = time;
-    //   table.rows[i].cells[3].append = frequency;
+    //   table.rows[i].cells[0].html = trainName;
+    //   table.rows[i].cells[1].html = destination;
+    //   table.rows[i].cells[2].html = time;
+    //   table.rows[i].cells[3].html = frequency;
     // }
     //clearing the local storage from previous visits
     localStorage.clear();
