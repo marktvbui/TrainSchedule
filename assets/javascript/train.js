12b5b4da675250e220930f586227dc9fccf44add
Mark Bui <tvyybui@yahoo.com>
2017-06-08 00:55:45 -0700

    removed un needed commented out code from my local host practice

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -8,7 +8,9 @@ $(document).ready(function(){
     messagingSenderId: "587854779697"
   }
   firebase.initializeApp(config);
+  // setting variables
   var database = firebase.database();
+  // setting initial variables to empty/0
   var trains = {};
   var trainName = '';
   var destination = '';
@@ -16,6 +18,7 @@ $(document).ready(function(){
   var frequency = 0;
   displayFirebase();
 
+  // on click event on the add-train button
   $("#add-train").on("click", function(event) {
     event.preventDefault();
     //setting variables, table to target table from index
@@ -51,64 +54,11 @@ $(document).ready(function(){
         row.append($('<td>').html(childSnapshot.val().trains.fbFrequency));
         $('#display-table').append(row);
         // console.log(snapshot.val());
-        // console.log(childSnapshot);
-    // }, function(errorObject) {
-    //   console.log('read failed: ' + errorObject);
       }), function(errorObject) {
         console.log('read failed: ' + errorObject);
       }
     });
   }
-    // console.log(trains.fbTrain);
-    // console.log(trains.fbDestination);
-    // displaying input fields into table, creating dynamic rows/columns
-    // row.append($('<td>').html(trainName));
-    // row.append($('<td>').html(destination));
-    // row.append($('<td>').html(time));
-    // row.append($('<td>').html(frequency));
-    // $('#display-table').append(row);
-    //clearing the local storage from previous visits
-    // localStorage.clear();
-    //setting local storage
-    // localStorage.setItem('localTrain', trainName);
-    // localStorage.setItem('localDestination', destination);
-    // localStorage.setItem('localTime', time);
-    // localStorage.setItem('localFrequency', frequency);
-    // trainList.push(trainName);
-    // trainlist.push(destination);
-    // trainlist.push(time);
-    // trainlist.push(frequency);
-    // trainList.push(trains);
-    // console.log(trainList);
-
-  // var value = ["aa","bb","cc"]
-  // will return: 'aa', 'bb', 'cc';
-  // localStorage.setItem('testkey', JSON.stringify(saveData));
-  // var test = JSON.parse(localStorage.getItem('trains'));
-  // console.log(saveData);
-  // console.log(test);
-    // var lTrain = localStorage.getItem('localTrain');
-    // var lDestination = localStorage.getItem('localDestination');
-    // var lTime = localStorage.getItem('localTime');
-    // var lfrequency = localStorage.getItem('localFrequency');
-
-    // console.log(lTrain);
-    // console.log(lDestination);
-    // console.log(lTime);
-    // console.log(lfrequency);
-    // looping through saveData to display localStorage info to table
-    // localStorage.setItem('test2', JSON.stringify(saveData));
-    // var test2 = JSON.parse(localStorage.getItem('test2'));
-    // looping through local storage
-    // for (i = 0; i < trainList.length; i++){
-    //   var row = $('<tr>');
-    //   //grabbing info from local storage and displaying it, even on refresh
-    //   row.append($('<td>').html(localStorage.getItem('localTrain')));
-    //   row.append($('<td>').html(localStorage.getItem('localDestination')));
-    //   row.append($('<td>').html(localStorage.getItem('localTime')));
-    //   row.append($('<td>').html(localStorage.getItem('localFrequency')));
-    //   $('#display-table').append(row);
-    // }
 });
 
 
